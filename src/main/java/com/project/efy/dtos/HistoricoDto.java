bd70957fadc737d1dbf787848a875130f19ee7a0
Davi <antoniodavidsouza13@gmail.com>
2026-06-12 21:09:27 -0300

    Adicao do Campo email em historico

--- src/main/java/com/project/efy/dtos/HistoricoDto.java
+++ src/main/java/com/project/efy/dtos/HistoricoDto.java
@@ -3,7 +3,7 @@ package com.project.efy.dtos;
 import java.util.Date;
 
 public record HistoricoDto(Integer id, String assunto,
-                           String message,String nomeUsuario, String destinatarios,
+                           String message,String nomeDoRemetente,String emailDoRemetente, String destinatarios,
                            String status, Date created_at, Date send_at, String error_message) {
 }
 
